81c0fb59f37e09315d8d6c3a6bc569dc265ae5c9
Tejas Chheda <tejas4888@gmail.com>
2017-08-19 00:14:07 +0530

    Added recycler adapter and recycleritem xml for reviews

---
+++ app/src/main/java/spit/matrix2017/HelperClasses/ReviewAdapter.java
@@ -0,0 +1,63 @@
+package spit.matrix2017.HelperClasses;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import spit.matrix2017.R;
+
+/**
+ * Created by Tejas on 19-08-2017.
+ */
+
+public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder> {
+
+    ArrayList<Feedback> feedbacks;
+    Context context;
+
+    ReviewAdapter(ArrayList<Feedback> feedbacks,Context context)
+    {
+        this.feedbacks=feedbacks;
+        this.context=context;
+    }
+
+    @Override
+    public ReviewAdapter.ReviewViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.reviewitem, parent, false);
+        return new ReviewViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ReviewAdapter.ReviewViewHolder holder, int position) {
+
+        //Perform operation here
+        //Eg. holder.emailtext.setText("abc@gmail.com");
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return feedbacks.size();
+    }
+
+    static class ReviewViewHolder extends RecyclerView.ViewHolder{
+
+        TextView emailtext,ratingtext,feedbacktext;
+        CardView reviewcontainer;
+
+        public ReviewViewHolder(View itemView) {
+            super(itemView);
+
+            emailtext=(TextView)itemView.findViewById(R.id.review_email);
+            ratingtext=(TextView)itemView.findViewById(R.id.review_rating);
+            feedbacktext=(TextView)itemView.findViewById(R.id.review_review);
+            reviewcontainer=(CardView)itemView.findViewById(R.id.review_cardcontainer);
+        }
+    }
+}
