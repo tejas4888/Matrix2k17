08026ffb7df3f60c25a97a4d72b9732bce606ab9
Tejas Chheda <tejas4888@gmail.com>
2017-08-18 19:59:39 +0530

    Add event split to two screens. Functionally ready

--- app/src/main/java/spit/matrix2017/Activities/AddEvent.java
+++ app/src/main/java/spit/matrix2017/Activities/AddEvent.java
@@ -44,7 +44,7 @@ public class AddEvent extends AppCompatActivity {
         ename = (EditText) findViewById(R.id.eventName);
         edesc = (EditText) findViewById(R.id.eventDescription);
         edate = (EditText) findViewById(R.id.eventDates);
-        etime = (EditText) findViewById(R.id.eventTime);
+        /*etime = (EditText) findViewById(R.id.eventTime);
         evenue = (EditText) findViewById(R.id.eventVenue);
         eOrgMail = (EditText) findViewById(R.id.eventOrgMail);
         epocName1 = (EditText) findViewById(R.id.eventPocName1);
@@ -53,7 +53,7 @@ public class AddEvent extends AppCompatActivity {
         epocNumber2 = (EditText) findViewById(R.id.eventPocNumber2);
 
         eprize = (EditText) findViewById(R.id.eventPrizeScheme);
-        efees = (EditText) findViewById(R.id.eventFeeScheme);
+        efees = (EditText) findViewById(R.id.eventFeeScheme);*/
 
         poster = (ImageView) findViewById(R.id.eventPoster);
 
@@ -108,22 +108,30 @@ public class AddEvent extends AppCompatActivity {
         name = ename.getText().toString();
         desc = edesc.getText().toString();
         date = edate.getText().toString();
-        time = etime.getText().toString();
-        venue = evenue.getText().toString();
+        //time = etime.getText().toString();
+        /*venue = evenue.getText().toString();
         orgMail = eOrgMail.getText().toString();
         pocName1 = epocName1.getText().toString();
         pocNumber1 = epocNumber1.getText().toString();
         pocName2 = epocName2.getText().toString();
         pocNumber2 = epocNumber2.getText().toString();
         prizeScheme = eprize.getText().toString();
-        feeScheme = efees.getText().toString();
+        feeScheme = efees.getText().toString();*/
 
         /*Hardcoded Poster*/
         if(url == null) {
             url = "https://s-media-cache-ak0.pinimg.com/originals/8e/f5/9d/8ef59dc3c90a3abd56c87a5901709132.jpg";
         }
-        mDatabaseReference.push().setValue(new Event(name,desc,url,date,time,venue,orgMail,pocName1,pocName2,pocNumber1,pocNumber2,prizeScheme,feeScheme));
-        Toast.makeText(this,"Done!",Toast.LENGTH_SHORT);
+        //mDatabaseReference.push().setValue(new Event(name,desc,url,date,time,venue,orgMail,pocName1,pocName2,pocNumber1,pocNumber2,prizeScheme,feeScheme));
+        //Toast.makeText(this,"Done!",Toast.LENGTH_SHORT);
+
+        Intent intent=new Intent(AddEvent.this,AddEvent2.class);
+        intent.putExtra("name",name);
+        intent.putExtra("url",url);
+        intent.putExtra("desc",desc);
+        intent.putExtra("date",date);
+        startActivity(intent);
+
         finish();
     }
 
---
+++ app/src/main/java/spit/matrix2017/Activities/AddEvent2.java
@@ -0,0 +1,63 @@
+package spit.matrix2017.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import spit.matrix2017.HelperClasses.Event;
+import spit.matrix2017.R;
+
+public class AddEvent2 extends AppCompatActivity {
+
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mDatabaseReference;
+    String timings,venue,mail,name1,num1,name2,num2,prize,fee;
+    String name,url,desc,date;
+    EditText etimings,evenue,email,ename1,enum1,ename2,enum2,eprize,efee;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event2);
+
+        etimings=(EditText)findViewById(R.id.event2_timings);
+        evenue=(EditText)findViewById(R.id.event2_venue);
+        email=(EditText)findViewById(R.id.event2_mail);
+        ename1=(EditText)findViewById(R.id.event2_name1);
+        enum1=(EditText)findViewById(R.id.event2_num1);
+        ename2=(EditText)findViewById(R.id.event2_name2);
+        enum2=(EditText)findViewById(R.id.event2_num2);
+        eprize=(EditText)findViewById(R.id.event2_prize);
+        efee=(EditText)findViewById(R.id.event2_fee);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("Events");
+    }
+
+    public void addEvent2(View v)
+    {
+        Intent intent=getIntent();
+        name=intent.getStringExtra("name");
+        url=intent.getStringExtra("url");
+        desc=intent.getStringExtra("desc");
+        date=intent.getStringExtra("date");
+
+        timings=etimings.getText().toString();
+        venue=evenue.getText().toString();
+        mail=email.getText().toString();
+        name1=ename1.getText().toString();
+        num1=enum1.getText().toString();
+        name2=ename2.getText().toString();
+        num2=enum2.getText().toString();
+        prize=eprize.getText().toString();
+        fee=efee.getText().toString();
+
+        mDatabaseReference.push().setValue(new Event(name,desc,url,date,timings,venue,mail,name1,name2,num1,num2,prize,fee));
+        finish();
+    }
+}
